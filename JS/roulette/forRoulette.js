2890dff69a52a5445f9ce23e1e4a4b090eb68a73
sjo <p@p>
2023-06-13 16:14:23 +0200

    GitHUB EinesAula

--- JS/roulette/forRoulette.js
+++ JS/roulette/forRoulette.js
@@ -2,27 +2,32 @@ let container = document.getElementById('container_list')
 let nombres = [];
 let lista = document.createElement('ul');
 let indice = 0;
-
-let fNombres ='names.txt'   // L'opció predeteminada, és la que es troba a la línia 4 del fitxer
 const tiempoRuleta=8000     // Temps que dona volta la ruleta en milisegons.
 
 const jsConfetti = new JSConfetti()
 const audioRun = document.getElementById("runRuolette");
 const audioWinner = document.getElementById("winner");
 
-fetch(fNombres)
-    .then(response => response.text())
-    .then(data => {
-        nombres = data.split('\n').filter(Boolean);
-        nombres.forEach(nombre => {
-            let elemento = document.createElement('li');
-            let texto = document.createTextNode(nombre);
-            elemento.appendChild(texto);
-            lista.appendChild(elemento);
-        });
-        container.appendChild(lista);
-    })
-    .catch(error => console.error(error));
+function loadNames(){ //JAB 20230612 Convertir a funció
+    // L'opció predeteminada, és la que es troba a la línia 4 del fitxer
+    //let fNombres ='grupA.txt'   //JAB 20230612
+    const grupTriat = document.getElementById("grup-dropdown"); //JAB 20230612
+    let fNombres= grupTriat.value //JAB 20230612
+
+    fetch(fNombres)
+        .then(response => response.text())
+        .then(data => {
+            nombres = data.split('\n').filter(Boolean);
+            nombres.forEach(nombre => {
+                let elemento = document.createElement('li');
+                let texto = document.createTextNode(nombre);
+                elemento.appendChild(texto);
+                lista.appendChild(elemento);
+            });
+            container.appendChild(lista);
+        })
+        .catch(error => console.error(error));
+}
 
 function spindNames() {
     indice = (indice + nombres.length - 1) % nombres.length;
@@ -34,6 +39,7 @@ function spindNames() {
 
 let intervalId = null;
 function startSpin() {
+    loadNames() //JAB20230612
     audioRun.play();
     restartRullet();
     nombres.sort(() => Math.random() - 0.5);
@@ -57,3 +63,5 @@ function stopWinner() {
     })
 }
 document.getElementById("rotar").addEventListener("click", startSpin);
+document.getElementById("grup-dropdown").addEventListener("click", loadNames); //JAB 20230612
+
